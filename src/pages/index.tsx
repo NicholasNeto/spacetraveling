7fb562f2afb0770501d5a6aac8312b81cfaa5b32
NicholasNeto <nicholasneto@outlook.com>
2021-04-21 13:40:22 -0300

    show button if next_page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
-
+import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom'
 
 interface Post {
   uid?: string;
@@ -24,26 +26,81 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+
+
+  //   <main className={styles.container}>
+  //   <div className={styles.posts}>
+  //       {posts.map(post => (
+  //           <Link href={`/posts/preview/${post.slug}`} key={post.slug}>
+  //               <a >
+  //                   <time>{post.updatedAt}</time>
+  //                   <strong>{post.title}</strong>
+  //                   <p>{post.excerpt}</p>
+  //               </a>
+  //           </Link>
+  //       ))}
+  //   </div>
+  // </main>
+
+
+
+
   return (
     <>
-      <h1> spacetraveling</h1>
-      <img src="/Logo.svg" alt="logo"/>
-      <div>
-        <div>Como utilizar Hooks</div>
-        <div>Pensando em sincronização em vez de ciclos de vida</div>
-        <div>Data</div>
-        <div>Nicholas Neto</div>
-      </div>
-
-      <div> Carregar mais posts</div>
+      <main className={styles.container}>
+        <img src="/Logo.svg" alt="logo" />
+        <div className={styles.posts}>
+          {postsPagination.results.map(post => (
+            <Link href={`/post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div>
+                  <time>{post.first_publication_date}</time>
+                  <span>{post.data.author}</span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+        {postsPagination.next_page && <div> Carregar mais posts</div>}
+      </main>
     </>
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.Predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    pageSize: 100,
+  });
 
-//   // TODO
-// };
+  const results = postsResponse.results.map((post: Post) => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(post.first_publication_date).toLocaleDateString(
+        'pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+      }
+    }
+  })
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        results
+      }
+    }
+  }
+};
