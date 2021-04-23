ce4cf6931d0745157f03a5c28fae500daf68ddc1
NicholasNeto <nicholasneto@outlook.com>
2021-04-22 23:49:17 -0300

    add logo in post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
+import React from 'react';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,20 +29,59 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <div>
+      <Header></Header>
+      <img src="/Logo.svg" alt="logo" />
+      teste
+    </div>
+  )
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
 
-//   // TODO
-// };
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+  // TODO
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async context => {
+
+  const { slug } = context.params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const resultContent = response.data.content.map(it => {
+    return {
+      heading: it.heading,
+      body: it.body.map(item => item.text)
+    }
+  })
+
+  const post = {
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: resultContent,
+    }
+  }
+
+  // console.log('888888', JSON.stringify(post, null, 2))
+
+  return {
+    props: {
+      post
+    },
+    redirect: 60 * 30 // 30 minutos
+  }
+};
