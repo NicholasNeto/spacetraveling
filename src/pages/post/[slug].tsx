bf12e330a5a08fa944ae7b761563ce63be91e598
NicholasNeto <nicholasneto@outlook.com>
2021-04-25 19:23:08 -0300

    add fix test for content

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -21,9 +21,10 @@ import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
+  uid: string;
   first_publication_date: string | null;
-  readTime: number,
   data: {
+    subtitle: string;
     title: string;
     banner: {
       url: string;
@@ -50,7 +51,23 @@ export default function Post({ post }: PostProps) {
   }
 
 
-  debugger
+
+  const resultContent = post.data.content.map(it => {
+    let bodyHtml = PrismicDOM.RichText.asHtml(it.body)
+    return {
+      body: bodyHtml,
+      heading: it.heading
+    }
+  })
+
+
+
+  let words = post.data.content.reduce((resultSoFar, current) => {
+    return [...resultSoFar, ...PrismicDOM.RichText.asText(current.body).split(" ")]
+  }, [])
+
+  let readingTime = Math.round(words.length / 200)
+
 
   return (
     <>
@@ -70,11 +87,11 @@ export default function Post({ post }: PostProps) {
           </div>
           <div>
             <BiTime />
-            <span>{`${post.readTime} min`}</span>
+            <span>{`${readingTime} min`}</span>
           </div>
         </div>
 
-        {post.data.content.map(it => {
+        {resultContent.map(it => {
           return (
             <div key={it.heading}>
               <h2 >{it.heading}</h2>
@@ -119,46 +136,29 @@ export const getStaticProps: GetStaticProps = async context => {
   const prismic = getPrismicClient();
   const response = await prismic.getByUID('posts', String(slug), {});
 
-  let palavras = response.data.content.reduce((resultSoFar, current) => {
-    return [...resultSoFar, ...PrismicDOM.RichText.asText(current.body).split(" ")]
-  }, [])
-
-  let time = Math.round(palavras.length / 200)
-  console.log(palavras.length)
-  console.log('time', time)
-
-
-  // let palavras = response.data.content[0].body.reduce((resultSoFar, current) => {
-  //   return [...resultSoFar, ...current.text.split(" ")]
-  // }, [])
-
-  const resultContent = response.data.content.map(it => {
-    let bodyHtml = PrismicDOM.RichText.asHtml(it.body)
-    return {
-      heading: it.heading,
-      body: bodyHtml
-    }
-  })
+  console.log('response', response)
 
   const post = {
+    uid: response.uid,
     first_publication_date: response.first_publication_date,
-    readTime: time,
     data: {
       title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
       banner: {
         url: response.data.banner.url,
       },
-      author: response.data.author,
-      content: resultContent,
+      content: response.data.content,
     }
   }
+
 
   console.log(JSON.stringify(post, null, 2))
 
 
   return {
     props: {
-      post
+      post,
     },
     redirect: 60 * 30 // 30 minutos
   }
