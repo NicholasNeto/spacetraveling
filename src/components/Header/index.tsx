e68ed5ff8ca7ba50e5b19a11938ecd003dd1b36f
NicholasNeto <nicholasneto@outlook.com>
2021-04-26 21:07:46 -0300

    container for Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import styles from './header.module.scss'
 
 export default function Header() {
   return (
-    <>
+    <div className={styles.container}>
       <Link href={"/"}>
         <a>
           <img src="/Logo.svg" alt="logo" />
         </a>
       </Link>
-    </>
+    </div>
   )
 }
