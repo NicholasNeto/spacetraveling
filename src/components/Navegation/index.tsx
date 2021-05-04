ed578e142a3b248d5cb78f2991c6e11d41b51511
NicholasNeto <nicholasneto@outlook.com>
2021-05-03 23:58:57 -0300

    create component navegation

---
+++ src/components/Navegation/index.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import styles from './navegation.module.scss'
+
+export default function Navegation({ title, path, label}) {
+    return (
+        <>
+            <div>{title}</div>
+            <Link href={`/post/${path}`}>
+                <a> {label}</a>
+            </Link>
+        </>
+    )
+}
+
