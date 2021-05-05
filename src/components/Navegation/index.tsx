f8041b973b2cb7a73b84b39d73901bdf04f7c923
NicholasNeto <nicholasneto@outlook.com>
2021-05-04 21:43:34 -0300

    add div for container

--- src/components/Navegation/index.tsx
+++ src/components/Navegation/index.tsx
@@ -1,14 +1,13 @@
 import Link from "next/link";
 import styles from './navegation.module.scss'
 
-export default function Navegation({ title, path, label}) {
+export default function Navegation({ title, path, label }) {
     return (
-        <>
+        <div className={styles.container}>
             <div>{title}</div>
             <Link href={`/post/${path}`}>
                 <a> {label}</a>
             </Link>
-        </>
+        </div>
     )
 }
-
