9b18dae0e152de76849586d4d6b9947ef1567b7e
NicholasNeto <nicholasneto@outlook.com>
2021-05-08 14:44:28 -0300

    config modal preview

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,33 +1,30 @@
-// import { getPrismicClient  } from "../../services/prismic"
+import { getPrismicClient } from "../../services/prismic"
+import { Document } from '@prismicio/client/types/documents';
 
 
-// import { Document } from '@prismicio/client/types/documents';
-// function linkResolver(doc: Document): string {
-//   if (doc.type === 'posts') {
-//     return `/post/${doc.uid}`;
-//   }
-//   return '/';
-// }
+function linkResolver(doc: Document): string {
+    if (doc.type === 'posts') {
+        return `/post/${doc.uid}`;
+    }
+    return '/';
+}
 
-// export default async (req, res) => {
-//   const { token: ref, documentId } = req.query;
+export default async (req, res) => {
+    const { token: ref, documentId } = req.query;
+    const redirectUrl = await getPrismicClient(req)
+        .getPreviewResolver(ref, documentId)
+        .resolve(linkResolver, '/');
 
-//   const prismic = getPrismicClient();
+    if (!redirectUrl) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
 
-//   const redirectUrl = await Client(req)
-//     .getPreviewResolver(ref, documentId)
-//     .resolve(linkResolver, '/');
+    res.setPreviewData({ ref });
 
-//   if (!redirectUrl) {
-//     return res.status(401).json({ message: 'Invalid token' });
-//   }
-
-//   res.setPreviewData({ ref });
-
-//   res.write(
-//     `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
-//     <script>window.location.href = '${redirectUrl}'</script>
-//     </head>`
-//   );
-//   res.end();
-// };
+    res.write(
+        `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${redirectUrl}" />
+    <script>window.location.href = '${redirectUrl}'</script>
+    </head>`
+    );
+    res.end();
+};
